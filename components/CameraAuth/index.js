defdc838821b274a9a9bfd5cb32a39cec87ee9a0
Bin <bin@cacdi.com>
2022-09-05 14:03:41 +0800

    asd

--- components/CameraAuth/index.js
+++ components/CameraAuth/index.js
@@ -7,9 +7,7 @@ import Android from "./Android";
 import styles from "./index.module.scss";
 const CameraAuth = ({ language }) => {
   const phoneType = useCheckPhoneType();
-  useEffect(() => {
-    window.alert(phoneType);
-  }, [phoneType]);
+  useEffect(() => {}, [phoneType]);
   return (
     <div className={styles.cameraAuth}>
       {phoneType == "IOS" && <IOS language={language} />}
